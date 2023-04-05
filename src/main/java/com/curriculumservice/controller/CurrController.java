26aef37b69639c03487d9b12380a6939b87fc13a
kdongk <ldh5081@naver.com>
2023-04-05 17:44:17 +0900

    Initialize Project

---
+++ src/main/java/com/curriculumservice/controller/CurrController.java
@@ -0,0 +1,13 @@
+package com.curriculumservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrController {
+
+    @GetMapping("/")
+    public String hello() {
+        return "안녕 난 커리큘럼 서비스야!";
+    }
+}
