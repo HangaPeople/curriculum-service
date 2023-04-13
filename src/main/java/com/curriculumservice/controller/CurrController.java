c97482a4000f7b804198a734d3e76587c91b0043
kdongk <ldh5081@naver.com>
2023-04-13 18:41:47 +0900

    fix: hello 메소드 엔드포인트 변경

--- src/main/java/com/curriculumservice/controller/CurrController.java
+++ src/main/java/com/curriculumservice/controller/CurrController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/curr")
 public class CurrController {
 
-    @GetMapping("/")
+    @GetMapping
     public String hello() {
         return "안녕 난 커리큘럼 서비스야!";
     }
